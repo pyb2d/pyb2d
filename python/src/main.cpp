84029992cb71836131b861ab0f51585d43ffa4ba
DerThorsten <derthorstenbeier@gmail.com>
2022-01-02 22:33:16 +0100

    bumped by hand

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -77,5 +77,5 @@ PYBIND11_MODULE(_b2d , pybox2dModule)
     exportEmitter(pybox2dModule);
     #endif
 
-    pybox2dModule.attr("__version__") = "0.5.2";
+    pybox2dModule.attr("__version__") = "0.6.0";
 }
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from pybind11 import get_cmake_dir, get_include
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup, find_packages
 
-__version__ = "0.5.2"
+__version__ = "0.6.0"
 
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.
